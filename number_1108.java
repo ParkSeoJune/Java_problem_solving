5e81f6dfd6cfbf03364efa84c5e121f70668a236
박서준 <83441545+ParkSeoJune@users.noreply.github.com>
2021-09-06 15:23:08 +0900

    Add files via upload

---
+++ number_1108.java
@@ -0,0 +1,11 @@
+package codeup;
+
+public class number_1108 {
+    public static void main(String[] args) {
+        for (int i = 0; i<50; i++){
+            if(i<20) System.out.print("hello");
+            else if(i==20) System.out.print(" ");
+            else System.out.print("world");
+        }
+    }
+}
