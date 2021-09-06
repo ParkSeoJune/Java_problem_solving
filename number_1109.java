a1d62c7d8a40e0864e45f31003164ce0624546d6
박서준 <83441545+ParkSeoJune@users.noreply.github.com>
2021-09-06 15:23:18 +0900

    Add files via upload

---
+++ number_1109.java
@@ -0,0 +1,20 @@
+package codeup;
+import java.util.Scanner;
+public class number_1109 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String x = in.nextLine();
+        int y = in.nextInt();
+        char z = in.next().charAt(0);
+        double u = in.nextDouble();
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(u);
+    }
+}
+// mark
+//20
+//A
+//3.154
