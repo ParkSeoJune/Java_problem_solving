5e8fda50d7782475ed9cd3b2fad1c8ca2ee5e40c
박서준 <83441545+ParkSeoJune@users.noreply.github.com>
2021-09-06 15:23:30 +0900

    Add files via upload

---
+++ number_1110.java
@@ -0,0 +1,10 @@
+package codeup;
+import java.util.Scanner;
+public class number_1110 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println(x);
+    }
+}
+// 이 문제는 어떤 숫자를 입력 받아서 출력하는 문제입니다.
