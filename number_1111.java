a550eb7c778a351f73a1bc14831fe8ec782af35d
박서준 <83441545+ParkSeoJune@users.noreply.github.com>
2021-09-07 19:33:03 +0900

    Add files via upload

---
+++ number_1111.java
@@ -0,0 +1,10 @@
+package codeup;
+import java.util.Scanner;
+public class number_1111 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.printf("%d%%", x);
+    }
+}
+// 한 정수가 입력되고 출력을 (숫자)%로 나오게 출력하는 문제
