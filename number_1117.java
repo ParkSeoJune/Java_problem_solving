744e948eccd3fed5c8bfd539b5027268c41d0bd1
박서준 <83441545+ParkSeoJune@users.noreply.github.com>
2021-09-08 20:05:12 +0900

    Add files via upload

---
+++ number_1117.java
@@ -0,0 +1,11 @@
+package codeup;
+
+import java.util.Scanner;
+public class number_1117 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+        System.out.printf("%.2f", x*y);
+    }
+}
