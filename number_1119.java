b1af09b5df05e382bb78d1d82aa9444c2b41d905
박서준 <83441545+ParkSeoJune@users.noreply.github.com>
2021-09-09 10:39:02 +0900

    Add files via upload

---
+++ number_1119.java
@@ -0,0 +1,11 @@
+package codeup;
+
+import java.util.Scanner;
+public class number_1119 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println(x*24);
+    }
+}
+// 일(Day)를 입력받아 시간으로 출력하는 문제
