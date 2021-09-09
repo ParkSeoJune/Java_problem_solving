79a6e40be572c2dc2ef8aafc639cda4349f56134
박서준 <83441545+ParkSeoJune@users.noreply.github.com>
2021-09-10 07:42:02 +0900

    Add files via upload

---
+++ number_1125.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+public class number_1125 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.printf("%o ", x);
+        System.out.printf("%X", x);
+    }
+}
+// 정수를 입력받아 8진수와 16진수(대문자)로 표시하는 문제
