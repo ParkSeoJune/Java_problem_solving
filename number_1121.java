0cc6b13e2d137dcdaeca3cbccf3d829a8bea6abb
박서준 <83441545+ParkSeoJune@users.noreply.github.com>
2021-09-09 10:39:34 +0900

    Add files via upload

---
+++ number_1121.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+public class number_1121 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int y = in.nextInt();
+        System.out.println(x%y);
+    }
+}
+// 두 수를 입력받아 나눈 값의 나머지를 구하는 문제
