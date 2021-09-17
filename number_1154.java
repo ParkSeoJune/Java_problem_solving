b24936b15d90661a6c7c3900c8ceca4b08e50be6
박서준 <83441545+ParkSeoJune@users.noreply.github.com>
2021-09-17 11:06:07 +0900

    Add files via upload

---
+++ number_1154.java
@@ -0,0 +1,11 @@
+package codeup;
+
+import java.util.Scanner;
+public class number_1154 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int y = in.nextInt();
+        System.out.println(Math.max(x, y) - Math.min(x, y));
+    }
+}
