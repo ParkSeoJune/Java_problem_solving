c3857fa844fd1a8523f241ce0d212cda20510be9
박서준 <83441545+ParkSeoJune@users.noreply.github.com>
2021-09-17 11:06:25 +0900

    Add files via upload

---
+++ number_1157.java
@@ -0,0 +1,11 @@
+package codeup;
+
+import java.util.Scanner;
+public class number_1157 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double x = in.nextDouble();
+        if(x>=50 && x <=60) System.out.println("win");
+        else System.out.println("lose");
+    }
+}
